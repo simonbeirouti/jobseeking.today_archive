07d0f55850e85f71de7c87d14d9809a8f3391588
simonbeirouti <hello@simonbeirouti.com>
2023-04-15 22:06:32 +1000

    make styling neumorphic

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -5,13 +5,13 @@ import Search from "@/components/ui/Search";
 export default function NavBar() {
   return (
     <div className="bg-white">
-      <header className="relative bg-white">
+      <header className="relative">
         <nav aria-label="Top" className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="px-4 pb-14 sm:px-0 sm:pb-0">
             <div className="flex h-16 items-center justify-between">
               <Link href="/">
                 <h1 className="tracking-widest font-light hidden md:inline uppercase text-lg">
-                  <span className="bg-slate-100 p-2 rounded-md">
+                  <span className="bg-gray-100 p-2 rounded-md shadow-light shadow-dark">
                     jobseeking<span className="text-gray-300">.today</span>
                   </span>
                 </h1>
@@ -22,10 +22,10 @@ export default function NavBar() {
               </div>
 
               <div className="flex items-center">
-                <div className="ml-4 flow-root bg-gray-300 p-2 rounded-md lg:ml-8">
+                <div className="ml-4 flow-root bg-white p-2 rounded-md lg:ml-8 shadow-light shadow-dark">
                   <a href="#" className="group -m-2 flex items-center p-2">
                     <UserCircleIcon
-                      className="h-6 w-6 flex-shrink-0 text-white"
+                      className="h-6 w-6 flex-shrink-0 text-gray-400 shadow-light-icon shadow-dark-icon"
                       aria-hidden="true"
                     />
                     <span className="sr-only">Account page</span>
--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -2,25 +2,28 @@ import { MagnifyingGlassIcon, BriefcaseIcon } from "@heroicons/react/20/solid";
 
 export default function Search() {
   return (
-    <div className="flex rounded-md shadow-sm w-full h-full">
+    <div className="flex rounded-md shadow-light shadow-dark bg-gray-100 w-full h-full">
       <div className="relative flex flex-grow items-stretch focus-within:z-10">
         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <BriefcaseIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <BriefcaseIcon
+            className="h-5 w-5 text-gray-400 shadow-light-icon shadow-dark-icon"
+            aria-hidden="true"
+          />
         </div>
         <input
           type="search"
           name="search"
           id="search"
-          className="block w-full rounded-none rounded-l-md border-0 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-l-md border-0 pl-10 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="Developer"
         />
       </div>
       <button
         type="submit"
-        className="relative -ml-px bg-gray-200 inline-flex items-center gap-x-1.5 rounded-r-md px-10 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        className="relative -ml-px bg-gray-100 inline-flex items-center gap-x-1.5 rounded-r-md px-10 text-sm font-semibold text-gray-900 shadow-light shadow-dark hover:bg-gray-500"
       >
         <MagnifyingGlassIcon
-          className="-ml-0.5 h-5 w-5 text-gray-400"
+          className="-ml-0.5 h-5 w-5 text-gray-400 shadow-light-icon shadow-dark-icon"
           aria-hidden="true"
         />
       </button>
