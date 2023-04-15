cdea6da393e988b72ba32a9870e01621de626c7b
simonbeirouti <hello@simonbeirouti.com>
2023-04-15 22:28:38 +1000

    change names in object

--- src/components/CategoryRow.jsx
+++ src/components/CategoryRow.jsx
@@ -2,50 +2,68 @@ import Image from "next/image";
 
 const files = [
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
+    category: "IMG_4985.HEIC",
+    jobs: "124",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    category: "IMG_4985.HEIC",
+    jobs: "124",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    category: "IMG_4985.HEIC",
+    jobs: "124",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    category: "IMG_4985.HEIC",
+    jobs: "124",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
@@ -53,15 +71,11 @@ const files = [
 
 export default function CategoryRow() {
   return (
-    <>
-      <h1 className="sm:pl-3 pl-4 text-lg uppercase">💼 Categories</h1>
-      <ul
-        role="list"
-        className="grid grid-cols-3 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-5 xl:gap-x-8"
-      >
+    <div className="overflow-x-auto -mt-[3.5rem] sm:mt-0 ">
+      <ul role="list" className="flex flex-no-wrap items-start space-x-4 p-2">
         {files.map((file, index) => (
-          <li key={index} className="relative p-2">
-            <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+          <li key={index} className="relative">
+            <div className="group aspect-h-7 aspect-w-10 block overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
               <Image
                 src={file.source}
                 alt=""
@@ -73,18 +87,20 @@ export default function CategoryRow() {
                 type="button"
                 className="absolute inset-0 focus:outline-none"
               >
-                <span className="sr-only">View details for {file.title}</span>
+                <span className="sr-only">
+                  View details for {file.category}
+                </span>
               </button>
             </div>
             <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-              {file.title}
+              {file.category}
             </p>
             <p className="pointer-events-none block text-sm font-medium text-gray-500">
-              {file.size}
+              {file.jobs}
             </p>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
