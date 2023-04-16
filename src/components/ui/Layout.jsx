131ae49012dd8ce47bf5467902fa067c3baf1ecd
simonbeirouti <hello@simonbeirouti.com>
2023-04-16 21:18:17 +1000

    create footer and add to layout

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/ui/NavBar";
+import Footer from "@/components/ui/Footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <NavBar />
       {children}
+      <Footer />
     </>
   );
 }
