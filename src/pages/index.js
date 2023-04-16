c32878034a7b2187965b2824dc611e5950a16364
simonbeirouti <hello@simonbeirouti.com>
2023-04-16 21:18:08 +1000

    Add CTA and rmv some spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import MetaHead from "@/components/ui/MetaHead";
 import CategoryRow from "@/components/CategoryRow";
 import TopJobs from "@/components/TopJobs";
 import LastChanceJobs from "@/components/LastChanceJobs";
+import CTA from "@/components/CTA";
 
 export default function Home() {
   return (
@@ -10,15 +11,16 @@ export default function Home() {
         page="Home"
         description="Discover your dream job with JobSeeking.today, the premier job search platform connecting you to top employers and exciting opportunities in your industry. Start your career journey now!"
       />
+      <div className="mx-auto max-w-3xl my-6">
+        <TopJobs />
+      </div>
       <div className="mx-auto max-w-6xl">
         <CategoryRow />
       </div>
-      <div className="mx-auto max-w-3xl my-6 sm:my-0">
-        <TopJobs />
-      </div>
-      <div className="mx-auto max-w-3xl my-6 sm:my-0">
+      <div className="mx-auto max-w-3xl my-6">
         <LastChanceJobs />
       </div>
+      <CTA />
     </>
   );
 }
