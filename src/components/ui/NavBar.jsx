6e5dec1f13f81fc8c4a00389a1e2d1621bcfa751
simonbeirouti <hello@simonbeirouti.com>
2023-04-17 22:32:11 +1000

    change styling to neubratalism

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -9,13 +9,14 @@ export default function NavBar() {
         <nav aria-label="Top" className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="px-4 sm:px-0 sm:pb-0">
             <div className="flex h-16 items-center justify-between">
-              <Link href="/" className="group">
-                <h1 className="tracking-widest font-light hidden md:inline uppercase text-lg ">
-                  <span className="bg-gray-100 p-2 rounded-md shadow-light shadow-dark group-hover:bg-gray-500 group-hover:text-white">
+              <Link
+                href="/"
+                className="group p-1 rounded-md border-2 hidden md:block border-black shadow-[2px_2px_0px_#000] hover:shadow-[4px_4px_0px_#000]"
+              >
+                <h1 className="tracking-widest font-light hidden md:inline uppercase text-lg">
+                  <span className="p-2">
                     jobseeking
-                    <span className="text-gray-300 group-hover:text-white">
-                      .today
-                    </span>
+                    <span className="text-gray-300">.today</span>
                   </span>
                 </h1>
               </Link>
@@ -25,10 +26,10 @@ export default function NavBar() {
               </div>
 
               <div className="flex items-center">
-                <div className="ml-4 flow-root bg-white p-2 rounded-md lg:ml-8 shadow-light shadow-dark hover:bg-gray-500 group">
-                  <a href="#" className="group -m-2 flex items-center p-2">
+                <div className="ml-4 flow-root border-2 border-black bg-red-500 shadow-[2px_2px_0px_#000] hover:shadow-[4px_4px_0px_#000] p-2 rounded-md lg:ml-8 hover:bg-red-400">
+                  <a href="#" className="group -m-2 flex items-center p-1.5">
                     <UserCircleIcon
-                      className="h-6 w-6 flex-shrink-0 text-gray-400 shadow-light-icon shadow-dark-icon group-hover:text-white"
+                      className="h-6 w-6 flex-shrink-0"
                       aria-hidden="true"
                     />
                     <span className="sr-only">Account page</span>
--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -2,30 +2,24 @@ import { MagnifyingGlassIcon, BriefcaseIcon } from "@heroicons/react/20/solid";
 
 export default function Search() {
   return (
-    <div className="flex rounded-md shadow-light shadow-dark bg-gray-100 w-full h-full">
-      <div className="relative flex flex-grow items-stretch focus-within:z-10">
+    <div className="flex rounded-md w-full h-full bg-light-gray">
+      <div className="relative flex flex-grow items-stretch focus-within:z-10 mr-1">
         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <BriefcaseIcon
-            className="h-5 w-5 text-gray-400 shadow-light-icon shadow-dark-icon"
-            aria-hidden="true"
-          />
+          <BriefcaseIcon className="h-5 w-5" aria-hidden="true" />
         </div>
         <input
           type="search"
           name="search"
           id="search"
-          className="block w-full rounded-l-md border-0 pl-10 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-2 outline-none border-black shadow-[2px_2px_0px_#000] pl-10 text-black placeholder:text-gray-400 sm:text-sm sm:leading-6"
           placeholder="Developer"
         />
       </div>
       <button
         type="submit"
-        className="relative -ml-px bg-gray-100 inline-flex items-center gap-x-1.5 rounded-r-md px-10 text-sm font-semibold text-gray-900 shadow-light shadow-dark hover:bg-gray-500 group"
+        className="ml-1 pointer bg-red-500 hover:bg-red-400 hover:transition-all shadow-[2px_2px_0px_#000] hover:shadow-[4px_4px_0px_#000] border-2 border-black relative inline-flex items-center gap-x-1.5 rounded-md px-10 text-sm font-semibold text-black hover:bg-medium-gray"
       >
-        <MagnifyingGlassIcon
-          className="-ml-0.5 h-5 w-5 text-gray-400 shadow-light-icon shadow-dark-icon group-hover:text-white"
-          aria-hidden="true"
-        />
+        <MagnifyingGlassIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
       </button>
     </div>
   );
