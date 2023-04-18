c8092192f363740966190e8a99b0db804ded2154
simonbeirouti <hello@simonbeirouti.com>
2023-04-19 01:36:50 +1000

    add supabase for auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
 import Layout from "@/components/ui/Layout";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionContextProvider>
   );
 }
