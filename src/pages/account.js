b69b31637161b05e768bf931430edbc5fc5a4d1e
simonbeirouti <hello@simonbeirouti.com>
2023-04-21 21:59:33 +1000

    create account page

---
+++ src/pages/account.js
@@ -0,0 +1,12 @@
+import MetaHead from "@/components/ui/MetaHead";
+
+export default function JobsPage() {
+  return (
+    <>
+      <MetaHead
+        page="Account"
+        description="The account page for jobseeking.today where you can edit your profile and manage your applications."
+      />
+    </>
+  );
+}
