1d1e650a7a46606ffeb6b72c18e50c24b9119f03
simonbeirouti <hello@simonbeirouti.com>
2023-04-25 22:06:46 +1000

    add greens

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       licorice: "#0D0106",
       polynesian: "#004E98",
       lapis: "#1D5E9F",
+      apple: "#93A117",
+      pear: "#C4D61F",
       chilli: "#E01300",
       vermillion: "#FB4437",
       gold: "#E5AB24",
