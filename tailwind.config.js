28a3b651fb28dc8e1556bb2a530c1ba72b302481
simonbeirouti <hello@simonbeirouti.com>
2023-04-25 21:51:07 +1000

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    colors: {
+      licorice: "#0D0106",
+      polynesian: "#004E98",
+      lapis: "#1D5E9F",
+      chilli: "#E01300",
+      vermillion: "#FB4437",
+      gold: "#E5AB24",
+      ecru: "#D5B772",
+      silver: "#CCCCCF",
+      white: "#FBFBFF",
+    },
     extend: {},
   },
   plugins: [
