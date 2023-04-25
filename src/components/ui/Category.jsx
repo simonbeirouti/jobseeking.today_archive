12c4629c2eb3e32a193fca22f06364001fc8a37d
simonbeirouti <hello@simonbeirouti.com>
2023-04-25 12:27:16 +1000

    create category button to reuse

---
+++ src/components/ui/Category.jsx
@@ -0,0 +1,44 @@
+import {
+  FaDumbbell,
+  FaRunning,
+  FaBookOpen,
+  FaMoneyBillWave,
+  FaCar,
+} from "react-icons/fa";
+
+const categoryIcons = {
+  developer: {
+    icon: <FaDumbbell className="category-icons" aria-hidden="true" />,
+  },
+  running: {
+    icon: <FaRunning className="category-icons" aria-hidden="true" />,
+  },
+  book: {
+    icon: <FaBookOpen className="category-icons" aria-hidden="true" />,
+  },
+  money: {
+    icon: <FaMoneyBillWave className="category-icons" aria-hidden="true" />,
+  },
+  car: {
+    icon: <FaCar className="category-icons" aria-hidden="true" />,
+  },
+};
+
+export default function Category({ category }) {
+  return (
+    <a
+      href={category.href}
+      className="shadow-style bg-orange-500 hover:bg-orange-400 relative flex h-56 w-56 flex-col justify-center items-center overflow-hidden rounded-md"
+    >
+      <span aria-hidden="true" className="mb-4">
+        {categoryIcons[category.category].icon}
+      </span>
+      <span className="relative text-center text-xl font-bold text-white">
+        {category.name}
+      </span>
+      <span className="relative text-center text-xl font-bold text-white">
+        {category.amount}
+      </span>
+    </a>
+  );
+}
