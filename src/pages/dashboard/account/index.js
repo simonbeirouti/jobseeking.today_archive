99c6675ad6a5b1777cba01a1e5d0cb290e9ec6d1
simonbeirouti <hello@simonbeirouti.com>
2023-04-26 22:46:06 +1000

    rmv extra account page and create under dash

--- src/pages/dashboard/account/index.js
+++ src/pages/dashboard/account/index.js
@@ -1,12 +1,16 @@
 import MetaHead from "@/ui/MetaHead";
+import Account from "@/components/Account";
+// import DashHeader from "@/components/ui/DashHeader";
 
-export default function JobsPage() {
+export default function AccountPage() {
   return (
     <>
       <MetaHead
         page="Account"
         description="The account page for jobseeking.today where you can edit your profile and manage your applications."
       />
+      {/* <DashHeader title="Account" /> */}
+      <Account />
     </>
   );
 }
