53e54a4de329fcea8785d392f1d000df12360177
simonbeirouti <hello@simonbeirouti.com>
2023-04-26 11:17:06 +1000

    fix styling and add about page text

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import MetaHead from "@/ui/MetaHead";
 import Header from "@/ui/Header";
 import CTA from "@/components/CTA";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,8 +12,67 @@ export default function Home() {
       />
       <Header
         title="About"
-        description="You'll discover the story behind the person you're curious about. From my humble beginnings to the lessons I've learned along the way, I invite you to join me on a journey that's sure to be filled with twists, turns, and plenty of inspiration."
+        description="Welcome to my about page, where you'll discover the story behind the person you're curious about. From my humble beginnings to the lessons I've learned along the way, I invite you to join me on a journey that's sure to be filled with twists, turns, and plenty of inspiration."
       />
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="bg-white shadow-style p-4 rounded-md mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-light leading-7 text-licorice">
+            DEPLOY FASTER
+          </p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-licorice sm:text-4xl">
+            A BETTER WORKFLOW
+          </h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-licorice lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                Id dolor praesent donec est. Odio penatibus risus viverra tellus
+                varius sit neque erat velit. Faucibus commodo massa rhoncus,
+                volutpat. Dignissim sed eget risus enim. Mattis mauris semper
+                sed amet vitae sed turpis id.
+              </p>
+              <p className="mt-8">
+                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
+                odio id et. Id blandit molestie auctor fermentum dignissim.
+                Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate
+                et ultrices hac adipiscing egestas.
+              </p>
+            </div>
+            <div>
+              <p>
+                Erat pellentesque dictumst ligula porttitor risus eget et eget.
+                Ultricies tellus felis id dignissim eget. Est augue maecenas
+                risus nulla ultrices congue nunc tortor.
+              </p>
+              <p className="mt-8">
+                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
+                odio id et. Id blandit molestie auctor fermentum dignissim.
+                Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate
+                et ultrices hac adipiscing egestas. Iaculis convallis ac tempor
+                et ut. Ac lorem vel integer orci.
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex">
+            <Link
+              href="#"
+              className="rounded-md bg-chilli shadow-style px-3.5 py-2.5 text-sm font-semibold text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              GET STARTED
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* <div className="relative overflow-hidden pt-16 lg:pt-20">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <img
+              className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+              src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+              alt=""
+            />
+          </div>
+        </div> */}
       <CTA />
     </>
   );
--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -12,146 +12,94 @@ export default function PrivacyPage() {
         title="Privacy policy"
         description="Our privacy policy outlines how we protect your personal information and ensure your privacy. By using our website, you can trust that your data is secure and we comply with data protection laws. We value your privacy and take all necessary measures to prevent data breaches and other violations."
       />
-      <div className="max-w-lg md:max-w-3xl lg:max-w-5xl mx-auto space-y-4 bg-white p-10 rounded-md shadow-style">
-        <h1>Privacy Policy for jobseeking.today</h1>
-        <p>
-          At jobseekingtoday, accessible from https://jobseeking.today, one of
-          our main priorities is the privacy of our visitors. This Privacy
-          Policy document contains types of information that is collected and
-          recorded by jobseekingtoday and how we use it.
-        </p>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="bg-white shadow-style p-4 rounded-md mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <h1 className="text-2xl font-bold">
+            Privacy Policy for jobseeking.today
+          </h1>
+          <p className="pp-text">
+            At jobseekingtoday, accessible from https://jobseeking.today, one of
+            our main priorities is the privacy of our visitors. This Privacy
+            Policy document contains types of information that is collected and
+            recorded by jobseekingtoday and how we use it.
+          </p>
 
-        <h2>Log Files</h2>
+          <h2 className="pp-title">Log Files</h2>
 
-        <p>
-          jobseekingtoday follows a standard procedure of using log files. These
-          files log visitors when they visit websites. All hosting companies do
-          this and a part of hosting services&apos; analytics. The information
-          collected by log files include internet protocol (IP) addresses,
-          browser type, Internet Service Provider (ISP), date and time stamp,
-          referring/exit pages, and possibly the number of clicks. These are not
-          linked to any information that is personally identifiable. The purpose
-          of the information is for analyzing trends, administering the site,
-          tracking users&apos; movement on the website, and gathering
-          demographic information.
-        </p>
+          <p className="pp-text">
+            jobseekingtoday follows a standard procedure of using log files.
+            These files log visitors when they visit websites. All hosting
+            companies do this and a part of hosting services&apos; analytics.
+            The information collected by log files include internet protocol
+            (IP) addresses, browser type, Internet Service Provider (ISP), date
+            and time stamp, referring/exit pages, and possibly the number of
+            clicks. These are not linked to any information that is personally
+            identifiable. The purpose of the information is for analyzing
+            trends, administering the site, tracking users&apos; movement on the
+            website, and gathering demographic information.
+          </p>
 
-        <h2>Cookies and Web Beacons</h2>
+          <h2 className="pp-title">Cookies and Web Beacons</h2>
 
-        <p>
-          Like any other website, jobseekingtoday uses &quot;cookies&quot;.
-          These cookies are used to store information including visitors&apos;
-          preferences, and the pages on the website that the visitor accessed or
-          visited. The information is used to optimize the users&apos;
-          experience by customizing our web page content based on visitors&apos;
-          browser type and/or other information.
-        </p>
+          <p className="pp-text">
+            Like any other website, jobseekingtoday uses &quot;cookies&quot;.
+            These cookies are used to store information including visitors&apos;
+            preferences, and the pages on the website that the visitor accessed
+            or visited. The information is used to optimize the users&apos;
+            experience by customizing our web page content based on
+            visitors&apos; browser type and/or other information.
+          </p>
 
-        {/* <h2>Google DoubleClick DART Cookie</h2>
+          <h2 className="pp-title">Third Party Privacy Policies</h2>
 
-        <p>
-          Google is one of a third-party vendor on our site. It also uses
-          cookies, known as DART cookies, to serve ads to our site visitors
-          based upon their visit to www.website.com and other sites on the
-          internet. However, visitors may choose to decline the use of DART
-          cookies by visiting the Google ad and content network Privacy Policy
-          at the following URL –{" "}
-          <a href="https://policies.google.com/technologies/ads">
-            https://policies.google.com/technologies/ads
-          </a>
-        </p>
+          <p className="pp-text">
+            jobseekingtoday&apos;s Privacy Policy does not apply to other
+            advertisers or websites. Thus, we are advising you to consult the
+            respective Privacy Policies of these third-party ad servers for more
+            detailed information. It may include their practices and
+            instructions about how to opt-out of certain options.{" "}
+          </p>
 
-        <h2>Our Advertising Partners</h2>
+          <p className="pp-text">
+            You can choose to disable cookies through your individual browser
+            options. To know more detailed information about cookie management
+            with specific web browsers, it can be found at the browsers&apos;
+            respective websites. What Are Cookies?
+          </p>
 
-        <p>
-          Some of advertisers on our site may use cookies and web beacons. Our
-          advertising partners are listed below. Each of our advertising
-          partners has their own Privacy Policy for their policies on user data.
-          For easier access, we hyperlinked to their Privacy Policies below.
-        </p>
+          <h2 className="pp-title">Children&apos;s Information</h2>
 
-        <ul>
-          <li>
-            <p>Google</p>
-            <p>
-              <a href="https://policies.google.com/technologies/ads">
-                https://policies.google.com/technologies/ads
-              </a>
-            </p>
-          </li>
-        </ul> 
+          <p className="pp-text">
+            Another part of our priority is adding protection for children while
+            using the internet. We encourage parents and guardians to observe,
+            participate in, and/or monitor and guide their online activity.
+          </p>
 
-        <h2>Privacy Policies</h2>
+          <p className="pp-text">
+            jobseekingtoday does not knowingly collect any Personal Identifiable
+            Information from children under the age of 13. If you think that
+            your child provided this kind of information on our website, we
+            strongly encourage you to contact us immediately and we will do our
+            best efforts to promptly remove such information from our records.
+          </p>
 
-        <p>
-          You may consult this list to find the privacy Policy for each of the
-          advertising partners of jobseekingtoday.
-        </p>
+          <h2 className="pp-title">Online Privacy Policy Only</h2>
 
-        <p>
-          Third-party ad servers or ad networks uses technologies like cookies,
-          JavaScript, or Web Beacons that are used in their respective
-          advertisements and links that appear on jobseekingtoday, which are
-          sent directly to users&apos; browser. They automatically receive your
-          IP address when this occurs. These technologies are used to measure
-          the effectiveness of their advertising campaigns and/or to personalize
-          the advertising content that you see on websites that you visit.
-        </p>
+          <p className="pp-text">
+            This Privacy Policy applies only to our online activities and is
+            valid for visitors to our website with regards to the information
+            that they shared and/or collect in jobseekingtoday. This policy is
+            not applicable to any information collected offline or via channels
+            other than this website.
+          </p>
 
-        <p>
-          Note that jobseekingtoday has no access to or control over these
-          cookies that are used by third-party advertisers.
-        </p> */}
+          <h2 className="pp-title">Consent</h2>
 
-        <h2>Third Party Privacy Policies</h2>
-
-        <p>
-          jobseekingtoday&apos;s Privacy Policy does not apply to other
-          advertisers or websites. Thus, we are advising you to consult the
-          respective Privacy Policies of these third-party ad servers for more
-          detailed information. It may include their practices and instructions
-          about how to opt-out of certain options.{" "}
-        </p>
-
-        <p>
-          You can choose to disable cookies through your individual browser
-          options. To know more detailed information about cookie management
-          with specific web browsers, it can be found at the browsers&apos;
-          respective websites. What Are Cookies?
-        </p>
-
-        <h2>Children&apos;s Information</h2>
-
-        <p>
-          Another part of our priority is adding protection for children while
-          using the internet. We encourage parents and guardians to observe,
-          participate in, and/or monitor and guide their online activity.
-        </p>
-
-        <p>
-          jobseekingtoday does not knowingly collect any Personal Identifiable
-          Information from children under the age of 13. If you think that your
-          child provided this kind of information on our website, we strongly
-          encourage you to contact us immediately and we will do our best
-          efforts to promptly remove such information from our records.
-        </p>
-
-        <h2>Online Privacy Policy Only</h2>
-
-        <p>
-          This Privacy Policy applies only to our online activities and is valid
-          for visitors to our website with regards to the information that they
-          shared and/or collect in jobseekingtoday. This policy is not
-          applicable to any information collected offline or via channels other
-          than this website.
-        </p>
-
-        <h2>Consent</h2>
-
-        <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its Terms and Conditions.
-        </p>
+          <p className="pp-text">
+            By using our website, you hereby consent to our Privacy Policy and
+            agree to its Terms and Conditions.
+          </p>
+        </div>
       </div>
     </>
   );
