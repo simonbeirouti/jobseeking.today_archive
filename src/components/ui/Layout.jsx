825052f5d30b26251b74b742bb9f3f39acb36b29
simonbeirouti <hello@simonbeirouti.com>
2023-04-26 19:33:55 +1000

    add router to switch layouts

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,12 +1,21 @@
 import NavBar from "@/components/ui/NavBar";
 import Footer from "@/components/ui/Footer";
+import { useRouter } from "next/router";
+import Dashboard from "./Dashboard";
 
 export default function Layout({ children }) {
+  const router = useRouter();
   return (
     <>
-      <NavBar />
-      {children}
-      <Footer />
+      {router.pathname === "/dashboard" ? (
+        <Dashboard>{children}</Dashboard>
+      ) : (
+        <>
+          <NavBar />
+          {children}
+          <Footer />
+        </>
+      )}
     </>
   );
 }
