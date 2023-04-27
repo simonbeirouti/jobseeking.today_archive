8cb1196f1c3e3cef38882fbf22ab127a3148d70d
simonbeirouti <hello@simonbeirouti.com>
2023-04-27 21:56:25 +1000

    add jobs to dash nav

--- src/components/ui/Dashboard.jsx
+++ src/components/ui/Dashboard.jsx
@@ -24,7 +24,7 @@ const navigation = [
     icon: UsersIcon,
     current: false,
   },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
+  { name: "Jobs", href: "/dashboard/jobs", icon: FolderIcon, current: false },
   { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
   { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
   { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
