e289d18169a66995502065ecd81c47d869ded95f
simonbeirouti <hello@simonbeirouti.com>
2023-04-28 12:21:31 +1000

    fix path for dashboard layout

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,7 +1,7 @@
 import NavBar from "@/components/ui/NavBar";
 import Footer from "@/components/ui/Footer";
 import { useRouter } from "next/router";
-import Dashboard from "./Dashboard";
+import Dashboard from "@/dashboard/Dashboard";
 
 export default function Layout({ children }) {
   const router = useRouter();
