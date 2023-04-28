e060dcf230b1d590998dc2fa7ccdcf9e7674b54d
simonbeirouti <hello@simonbeirouti.com>
2023-04-28 12:01:56 +1000

    integrate jobshortlist

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import MetaHead from "@/ui/MetaHead";
 import CategoryRow from "@/components/CategoryRow";
-import TopJobs from "@/components/TopJobs";
-import EndingSoon from "@/components/EndingSoon";
+import JobShortlist from "@/components/JobShortlist";
 import CTA from "@/components/CTA";
 
 export default function Home() {
@@ -12,13 +11,13 @@ export default function Home() {
         description="Discover your dream job with JobSeeking.today, the premier job search platform connecting you to top employers and exciting opportunities in your industry. Start your career journey now!"
       />
       <div className="mx-auto max-w-3xl my-6">
-        <TopJobs />
+        <JobShortlist type="ending" />
       </div>
       <div className="mx-auto max-w-6xl">
         <CategoryRow />
       </div>
       <div className="mx-auto max-w-3xl my-6">
-        <EndingSoon />
+        <JobShortlist type="top" />
       </div>
       <CTA />
     </>
