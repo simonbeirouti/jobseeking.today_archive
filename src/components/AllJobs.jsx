bba8918b061de9b5d6647cffa84409686977a63e
simonbeirouti <hello@simonbeirouti.com>
2023-04-28 11:58:37 +1000

    extract sorter and place it directly into alljobs

--- src/components/AllJobs.jsx
+++ src/components/AllJobs.jsx
@@ -1,5 +1,4 @@
 import Listing from "@/ui/Listing";
-import Sorter from "@/ui/Sorter";
 
 const positions = [
   {
@@ -33,11 +32,115 @@ const positions = [
 
 export default function AllJobs() {
   return (
-    <ul role="list">
-      <Sorter />
-      {positions.map((position) => (
-        <Listing position={position} key={position.id} />
-      ))}
-    </ul>
+    <>
+      <div class="flex flex-col mb-4 mx-1 md:flex-row md:flex-wrap">
+        <div className="md:w-1/5 mb-2 md:mb-0 px-2">
+          <label
+            htmlFor="location"
+            className="block text-sm font-medium leading-6 text-licorice"
+          >
+            LOCATION
+          </label>
+          <select
+            id="location"
+            name="location"
+            className="mt-1 block w-full rounded-md py-1.5 pl-3 pr-10 text-licorice shadow-style sm:text-sm sm:leading-6"
+            defaultValue="ALL"
+          >
+            <option>ALL</option>
+            <option>United States</option>
+            <option>Canada</option>
+            <option>Mexico</option>
+          </select>
+        </div>
+
+        <div className="md:w-1/5 mb-2 md:mb-0 px-2">
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium leading-6 text-licorice"
+          >
+            CATEGORY
+          </label>
+          <select
+            id="category"
+            name="category"
+            className="mt-1 block w-full rounded-md py-1.5 pl-3 pr-10 text-licorice shadow-style sm:text-sm sm:leading-6"
+            defaultValue="ALL"
+          >
+            <option>ALL</option>
+            <option>Technology</option>
+            <option>Marketing</option>
+            <option>Sales</option>
+          </select>
+        </div>
+
+        <div className="md:w-1/5 mb-2 md:mb-0 px-2">
+          <label
+            htmlFor="salary"
+            className="block text-sm font-medium leading-6 text-licorice"
+          >
+            SALARY
+          </label>
+          <select
+            id="salary"
+            name="salary"
+            className="mt-1 block w-full rounded-md py-1.5 pl-3 pr-10 text-licorice shadow-style sm:text-sm sm:leading-6"
+            defaultValue="ALL"
+          >
+            <option>ALL</option>
+            <option>$50,000 - $75,000</option>
+            <option>$75,000 - $100,000</option>
+            <option>$100,000+</option>
+          </select>
+        </div>
+
+        <div className="md:w-1/5 mb-2 md:mb-0 px-2">
+          <label
+            htmlFor="experience"
+            className="block text-sm font-medium leading-6 text-licorice"
+          >
+            WORK TYPE
+          </label>
+          <select
+            id="experience"
+            name="experience"
+            className="mt-1 block w-full rounded-md py-1.5 pl-3 pr-10 text-licorice shadow-style sm:text-sm sm:leading-6"
+            defaultValue="ALL"
+          >
+            <option>ALL</option>
+            <option>CASUAL</option>
+            <option>CONTRACT</option>
+            <option>PART-TIME</option>
+            <option>FULL-TIME</option>
+          </select>
+        </div>
+
+        <div className="md:w-1/5 mb-2 md:mb-0 px-2">
+          <label
+            htmlFor="experience"
+            className="block text-sm font-medium leading-6 text-licorice"
+          >
+            CLOSING IN
+          </label>
+          <select
+            id="experience"
+            name="experience"
+            className="mt-1 block w-full rounded-md py-1.5 pl-3 pr-10 text-licorice shadow-style sm:text-sm sm:leading-6"
+            defaultValue="ALL"
+          >
+            <option>ALL</option>
+            <option>TODAY</option>
+            <option>TOMORROW</option>
+            <option>IN 3 DAYS</option>
+            <option>OVER 7 DAYS</option>
+          </select>
+        </div>
+      </div>
+      <ul role="list" className="mx-3">
+        {positions.map((position) => (
+          <Listing position={position} key={position.id} />
+        ))}
+      </ul>
+    </>
   );
 }
